57f1cd63c0312943f532f7239a27cd40cbb69f0f
BradMcEwen <mcewen.brad30@gmail.com>
2024-08-12 01:04:28 -0400

    config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bradmcewen.github.io/',
+  base: '/BradMcEwen.github.io/',
   plugins: [react()],
   build: {
     outDir: 'dist',  // Output directory for the build files
